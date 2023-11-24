e317f239fd7405d4c3d74d0507faadcfffb0f522
CLopMan <cesarlopman@gmail.com>
2023-11-24 16:40:16 +0100

    primera version

---
+++ parte1/CSPParking.py
@@ -0,0 +1,99 @@
+from read_write import Read_Write
+
+from random import randint
+from constraint import *
+import sys
+
+def gen_matrix_one_solution(matrix: list, solution:dict, grid):
+    #print("one solution:", solution)
+    for i in range(grid[0]):
+        row = []
+        for j in range(grid[1]):
+            row.append("-")
+        matrix.append(row)
+    for ambulance in solution:
+        matrix[solution[ambulance][0] - 1][solution[ambulance][1] - 1] = ambulance
+
+def gen_output_data(solutions: list, grid: tuple):
+    impresas = []
+    super_matrix = []
+    for i in range(min(3, len(solutions))):
+        matrix = []
+        random = randint(0, len(solutions) - 1)
+        while random in impresas:
+            random = randint(0, len(solutions) - 1)
+        impresas.append(random)
+        gen_matrix_one_solution(matrix, solutions[random], grid)
+        matrix.append(list("*"*grid[1]))
+        super_matrix += matrix
+
+    first_row = ["Nº Sol:", len(solutions)]
+    super_matrix.insert(0, first_row)
+    return super_matrix
+    
+    
+
+
+def free_side(amb1, amb2):
+    if amb1[1] != amb2[1]:
+        return True
+    return amb1[0] != amb2[0] + 1 and amb1[0] != amb2[0] + 1
+
+def tsu_not_on_left(tnu: tuple, tsu:tuple):
+    if tnu[0] != tsu[0]:
+        return True
+    return tsu[1] > tnu[1]
+
+def set_constraints(problem:Problem, ambulances:dict):
+    # Every ambulance with other every ambulance
+    for i in ambulances:
+        for j in ambulances:
+            for e in ambulances[i]:
+                for ee in ambulances[j]:
+                    if e != ee:
+                        # All diferent
+                        problem.addConstraint(AllDifferentConstraint(), [e, ee])
+                        # all ambulance has to have a free place on its side
+                        problem.addConstraint(free_side, [e, ee])
+
+    # TNU cannot have tsu on left
+    tsus = ambulances["TSU-X"] + ambulances["TSU-C"]
+    tnus = ambulances["TNU-X"] + ambulances["TNU-C"]
+    for tnu in tnus:
+        for tsu in tsus:
+            problem.addConstraint(tsu_not_on_left, [tnu, tsu])
+
+def gen_domain_grid(grid):
+    domain = []
+    for i in range(1, grid[0] + 1):
+        for j in range(1, grid[1] + 1):
+            domain.append((i, j))
+    return domain
+
+def set_up_problem(problem: Problem, grid:tuple, ambulances: dict, pe:list):
+    complete_grid = gen_domain_grid(grid)
+
+    # Variables - initialization 
+    problem.addVariables(ambulances["TSU-C"], pe)
+    problem.addVariables(ambulances["TSU-X"], complete_grid)
+    problem.addVariables(ambulances["TNU-C"], pe)
+    problem.addVariables(ambulances["TNU-X"], complete_grid)
+
+    # constraints 
+    set_constraints(problem, ambulances)
+
+
+def main():
+    rw = Read_Write(sys.argv[1])
+    grid, pe, ambulances = rw.read()
+    
+    problem = Problem()
+    set_up_problem(problem, grid, ambulances, pe)
+
+    solutions = problem.getSolutions()
+    #print(solutions)
+
+    rw.write(gen_output_data(solutions, grid))
+
+if __name__=="__main__":
+    main()
---
+++ parte1/read_write.py
@@ -0,0 +1,64 @@
+import csv
+
+class Read_Write():
+    def __init__(self, in_path):
+        self.input_path: str = in_path
+        self.output_path: str = in_path + ".csv"
+
+
+    def read(self):
+        with open(self.input_path, "r") as file:
+            grid: tuple = self.read_grid_size(file)
+            pe: list = self.read_electric_places(file)
+            ambulances: dict = self.read_ambulances(file)
+        return grid, pe, ambulances
+        
+    def read_grid_size(self, file) -> tuple:
+        parse = ['x', '\n']
+        grid = [0, 0]
+        c = ''
+        for i in range(2):
+            power = 1
+            c = file.read(1)
+            while c != parse[i]:
+                grid[i] = grid[i]*power + int(c)
+                power = 10
+                
+                c = file.read(1)
+            if grid[i] == 0:
+                raise Exception("Bad input file: grid size")
+        return tuple(grid)
+    
+    def read_electric_places(self, file) -> list:
+        line = file.readline() # read a complete line
+        line = line[3:-1] # discards PE: and \n
+        if len(line) % 5 != 0:
+            raise Exception("Bad input file: PE incomplete")
+        places = [] 
+        for i in range(0, len(line), 5):
+            place = line[i: i + 5]
+            places.append((int(place[1]), int(place[3])))
+
+        return places
+    
+    def read_ambulances(self, file) -> dict:
+        out = {
+            "TSU-C": [],
+            "TSU-X": [],
+            "TNU-C": [],
+            "TNU-X": []
+        }
+
+        ambulance = None
+        while True:
+            prev_ambulance = ambulance
+            ambulance = file.readline()
+            if not ambulance:
+                return out
+            out[ambulance[2:-1]].append(ambulance[:-1])            
+            
+        
+    def write(self, data):
+        with open(self.output_path, "w") as ofile:
+            writer = csv.writer(ofile)
+            writer.writerows(data)
