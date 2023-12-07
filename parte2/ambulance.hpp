b2d9ac6cd5ee2ef7c5adb1666790545f1aa3c975
AdriExtremix <adrianfernandezgalan@gmail.com>
2023-12-07 13:07:26 +0100

    funciones de estado y ambulancia struct

--- parte2/ambulance.hpp
+++ parte2/ambulance.hpp
@@ -13,17 +13,15 @@ struct Position {
 };
 
 
-class Ambulance {
-private:
+struct Ambulance {
     Position position;
     int cont_contagioso; // los que van dentro
     int cont_no_contagioso; // los que van dentro
     int energy;
     int limit_x;
     int limit_y;
+    
     bool ev_pos(int x, int y);
-
-public: 
     Ambulance(int xParking, int yParking) : position{xParking, yParking}, cont_contagioso(0), cont_no_contagioso(0), energy(ENERGIA_INICIAL) {}
     Position get_position() {return position;};
     void move_right(); 
--- parte2/state.hpp
+++ parte2/state.hpp
@@ -30,4 +30,44 @@ struct State {
         if (map.get_slot(ambulance.get_position().x, ambulance.get_position().y).get_type() == hospital_nc) ambulance.drop_ncontagioso();
     };
 
+    // neighbors
+    std::vector<State> neighbors(Map map){
+        std::vector<State> neighbors;
+        std::vector<State> states(8);
+        for (int i = 0; i < 8;i++){
+            states[i] = *this;
+        }
+        states[0].move_right();
+        states[1].move_left();
+        states[2].move_up();
+        states[3].move_down();
+        states[4].pick_up_contagioso(map);
+        states[5].pick_up_ncontagioso(map);
+        states[6].drop_contagioso(map);
+        states[7].drop_ncontagioso(map);
+        for (int i = 0; i < 8;i++){
+            if (states[i] != *this){
+                neighbors.push_back(states[i]);
+            }
+        }
+        return neighbors;
+    };
+    // state to key
+    std::string to_string() {
+        std::string key = std::to_string(ambulance.get_position().x)+ "/" + std::to_string(ambulance.get_position().y) + "/" + std::to_string(ambulance.cont_contagioso) + "/" + std::to_string(ambulance.cont_no_contagioso) + "/" + std::to_string(ambulance.energy) + "/" + std::to_string(contagiosos) + "/" + std::to_string(no_contagiosos);
+        return key;
+    };
+
+    // operators
+    bool operator==(State & other) {
+        return {ambulance.get_position().x == other.ambulance.get_position().x && ambulance.get_position().y == other.ambulance.get_position().y 
+        && ambulance.cont_contagioso == other.ambulance.cont_contagioso && ambulance.cont_no_contagioso == other.ambulance.cont_no_contagioso 
+        && ambulance.energy == other.ambulance.energy && contagiosos == other.contagiosos && no_contagiosos == other.no_contagiosos};
+    };
+    bool operator!=(State & other) {
+        return {!(ambulance.get_position().x == other.ambulance.get_position().x && ambulance.get_position().y == other.ambulance.get_position().y 
+        && ambulance.cont_contagioso == other.ambulance.cont_contagioso && ambulance.cont_no_contagioso == other.ambulance.cont_no_contagioso 
+        && ambulance.energy == other.ambulance.energy && contagiosos == other.contagiosos && no_contagiosos == other.no_contagiosos)};
+    
+    };
 };
