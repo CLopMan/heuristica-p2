41e3e70dbd7b0b84095eeee8b020e04361261805
CLopMan <cesarlopman@gmail.com>
2023-12-08 21:35:42 +0100

    Movido shell script a parte1

---
+++ parte1/parte1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Yell='\033[1;33m'
+NC='\033[0m' # No Color
+
+head="test_cases/"
+for i in `ls test_cases`
+do
+    caso="$head$i"
+    echo -e "${Yell}TESTING:${NC} $caso\n"
+    python CSPParking.py $caso
+done
