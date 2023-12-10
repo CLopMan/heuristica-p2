b86fe7178ea82946beb7007a4043e0e522fb08e9
Cesar <cesarlopman@gmail.com>
2023-12-10 11:50:10 +0100

    comentarios y refactoring

--- parte1/CSPParking.py
+++ parte1/CSPParking.py
@@ -5,22 +5,23 @@ from constraint import *
 import sys
 import itertools
 
-NUMBER_OF_SOLUTIONS_MIN = 3
-grid_size: tuple = ()
+NUMBER_OF_SOLUTIONS_MIN = 3 # número de soluciones que se detallan
+grid_size: tuple = () # tamaño del mapa
 
 # ======================= print ==============================
+
+# matriz con una solución con el formato adecuado
 def gen_matrix_one_solution(matrix: list, solution:dict, grid):
-    #print("one solution:", solution)
     for i in range(grid[0]):
         row = []
         for j in range(grid[1]):
             row.append("-")
         matrix.append(row)
     for ambulance in solution:
-        #print("INDEX: ", solution[ambulance][0] - 1, solution[ambulance][1] - 1)
         if not ambulance[0] == '-':
             matrix[solution[ambulance][0] - 1][solution[ambulance][1] - 1] = ambulance
 
+# genera soluciones para escribir en el archivo de salida
 def gen_output_data(solutions: list, grid: tuple):
     impresas = []
     super_matrix = []
@@ -43,12 +44,7 @@ def gen_output_data(solutions: list, grid: tuple):
 
 # ====================== CONSTRAINTS ============================
 
-def eval_free_side_border(amb1, amb2):
-    if amb1[1] == amb2[1]:
-        return amb1[0] + 1 != amb2[0] and amb1[0] - 1 != amb2[0]
-    return True
-
-# 3 ambulances must not be continous in the same column
+# 3 ambulancias no pueden ser consecutivas en una misma columna
 def free_sidea3(amb1, amb2, amb3):
     x = [amb1[0], amb2[0], amb3[0]]
     x.sort()
@@ -57,41 +53,48 @@ def free_sidea3(amb1, amb2, amb3):
             return False 
     return True
 
-# two ambulances cannot be together next to a border
+# 2 ambulancias no pueden ser consecutivas cuando están en la misma columna y junto a un extremo
+def eval_free_side_border(amb1, amb2):
+    if amb1[1] == amb2[1]:
+        return amb1[0] + 1 != amb2[0] and amb1[0] - 1 != amb2[0]
+    return True
+
 def free_side_border(amb1, amb2):
     if (amb1[0] - 1 == 0 or amb1[0] == grid_size[0]):
             return eval_free_side_border(amb1, amb2)
     
     return True
 
-
+# Una tnu no puede tener una tsu detrás
 def tsu_not_on_left(tnu: tuple, tsu:tuple):
     if tnu[0] != tsu[0]:
         return True
     return tsu[1] > tnu[1]
 
+# añade las restricciones
 def set_constraints(problem:Problem, ambulances:dict):
-    # ambulances list
-    tsus = ambulances["TSU-X"] + ambulances["TSU-C"]
-    tnus = ambulances["TNU-X"] + ambulances["TNU-C"]
-    ambulances = tsus + tnus
+    
+    # listas de ambulancias
+    tsus = ambulances["TSU-X"] + ambulances["TSU-C"] # sólo tsu
+    tnus = ambulances["TNU-X"] + ambulances["TNU-C"] # sólo tnu
+    ambulances = tsus + tnus # todas
 
-    problem.addConstraint(AllDifferentConstraint())
+    problem.addConstraint(AllDifferentConstraint()) # todas diferentes
 
-
-    # Every ambulance with other every ambulance
     for i in itertools.permutations(ambulances, 2):
-        # All diferent
+        # plaza libre al lado cuando están junto a un borde
         problem.addConstraint(free_side_border, list(i))
     
+    for i in itertools.combinations(ambulances, 3):
+        # plaza libre al lado
+        problem.addConstraint(free_sidea3, list(i)) 
 
-    # TNU cannot have tsu on left
+    # Una tnu no puede tener una tsu detrás
     for tnu in tnus:
         for tsu in tsus:
             problem.addConstraint(tsu_not_on_left, [tnu, tsu])
 
-    for i in itertools.combinations(ambulances, 3):
-        problem.addConstraint(free_sidea3, list(i))
+    
 
 
 # =============================== Miscelaneous ======================
@@ -106,29 +109,34 @@ def gen_domain_grid(grid):
 # ================== problem inicialization =========================
 
 def set_up_problem(problem: Problem, grid:tuple, ambulances: dict, pe:list):
-    complete_grid = gen_domain_grid(grid)
+    complete_grid = gen_domain_grid(grid) # dominio de todas las posiciones
 
-    # Variables - initialization 
+    # Variables - inicialización 
     problem.addVariables(ambulances["TSU-C"], pe)
     problem.addVariables(ambulances["TSU-X"], complete_grid)
     problem.addVariables(ambulances["TNU-C"], pe)
     problem.addVariables(ambulances["TNU-X"], complete_grid)
+    
     # constraints 
     set_constraints(problem, ambulances)
 
 
 # ============================ Main ===================================
 def main():
+    # objeto encargado de la lectura y escritura
     rw = Read_Write(sys.argv[1])
+
+    # datos de lectura
     grid, pe, ambulances = rw.read()
     global grid_size
     grid_size = grid
     
     problem = Problem()
+
+    # inicialización del problema
     set_up_problem(problem, grid, ambulances, pe)
 
     solutions = problem.getSolutions()
-
     rw.write(gen_output_data(solutions, grid))
 
 if __name__=="__main__":
